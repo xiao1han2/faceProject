95b85f1d44d61ce28a53b1223c18491206cd991b
阮茂良 <134614082+xiao1han2@users.noreply.github.com>
2024-02-05 21:47:58 +0800

    Create setup.py

---
+++ src/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+from config import __VERSION__
+
+setup(
+    name='BioMime',
+    version=__VERSION__ ,
+    author='Ruan Maoliang',
+    author_email='2052024601@qq.com',
+    description='pattern recognition',
+
+    url='https://github.com/xiao1han2/faceProject',
+
+    packages=find_packages()
+)
